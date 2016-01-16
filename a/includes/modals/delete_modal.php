171419a52a28d0882ce8c25abe19b12ec32323c8
Kevin Mikolajczak <me@kevinmikol.com>
2016-01-16 17:37:30 -0600

    Rebuilt Modal

--- a/includes/modals/delete_modal.php
+++ a/includes/modals/delete_modal.php
@@ -1,14 +1,18 @@
 	<!-- Delete Modal -->
-	<div id="deleteModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
-	  <div class="modal-header">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-		<h3 id="myModalLabel"><i class="icon-trash"></i>  Deletion</h3>
-	  </div>
-	  <div class="modal-body">
-		<h4>Are you sure you want to delete <span id="type"></span> #<span id="id"></span> - <span id="title"></span>?</h4>
-	  </div>
-	  <div class="modal-footer">
-		<button class="btn" data-dismiss="modal" aria-hidden="true">Not now</button>
-		<button class="btn btn-primary deleteYes btn-danger"><i class="icon-remove-sign"></i>  Yes, delete</button>
-	  </div>
-	</div>
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title"><i class="fa fa-trash"></i>  Deletion</h4>
+            </div>
+            <div class="modal-body">
+                <p>Are you sure you want to <span class="label label-danger">permanently delete</span> the <span id="type"></span> <b><span id="title"></span></b>?</p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Not now</button>
+                <button class="btn btn-primary deleteYes btn-danger"><i class="icon-remove-sign"></i>  Yes, delete</button>
+            </div>
+        </div>
+    </div>
+</div>
