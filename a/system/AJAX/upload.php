c687a0748b071be4366c1f5551502b9a287bdd3f
Evan <me@kevinmikol.com>
2016-01-20 15:40:22 -0600

    Filename Fix

--- a/system/AJAX/upload.php
+++ a/system/AJAX/upload.php
@@ -53,7 +53,7 @@ if($_FILES){
                 $ext = pathinfo($file["name"], PATHINFO_EXTENSION);
                 $name = str_replace(".".$ext, '' , $file["name"]);
 
-                $file['name'] = $name."_".microtime().".".$ext;
+                $file['name'] = $name."_".str_replace('.', '', microtime(true)).".".$ext;
             }
             
 	        $filename = compress_image($file["tmp_name"], $url.$file['name'], 8, 800, 800);
